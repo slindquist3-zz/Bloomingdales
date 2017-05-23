67b3092ba23f1d8ca543e66387f7fb819390baa3
slindquist3 <slindquist3@gmail.com>
2017-05-23 14:25:55 -0400

    integrated bootstrap

--- js/main.js
+++ js/main.js
@@ -1,17 +1,33 @@
+//adding an argument to all of these functions
+	//will make them more resuable
+
+
 function total() {
 	var amount = parseInt(product.get("price").sale.slice(1));
-	var quantity = parseInt(product.get("quantity"));
+	var quantity = product.get("quantity");
 	return amount * quantity;
 }
 
+function getQuantity() {
+	return parseInt(product.get("quantity"));
+}
+
+function getSalePrice() {
+	return parseInt(product.get("price").sale.slice(1));
+}
+
+function getOriginalPrice() {
+	return parseInt(product.get("price").original.slice(1));
+}
+
 function savings() {
-	var originalPrice = parseInt(product.get("price").original.slice(1));
-	var quantity = parseInt(product.get("quantity"));
+	var originalPrice = getOriginalPrice()
+	var quantity = getQuantity()
 	var wouldBePrice = originalPrice * quantity;
 	return wouldBePrice - total();
 }
 
-//I can refactor the above two functions into one
+//I can refactor the above two functions into one, or atleast not repeat myself
 
 $('#next').on('click', function(e) {
 	e.preventDefault();
@@ -63,6 +79,9 @@ $(".color-selection").on("change", function() {
 	console.log(product.attributes);
 });
 
+//I can refactor this so that each image is just a value in an array and on color change the function iterates through the array of hypothetical images
+//These image values should all be stored on the instance of the model
+
 $(".quantity-selection").on("change", function() {
 	product.set(this.name, this.value);
 	console.log(product.attributes);
@@ -71,12 +90,27 @@ $(".quantity-selection").on("change", function() {
 // ---------  MODELS -------------
 
 var Product = Backbone.Model.extend({
+		validate: function(attrs) {
+			if(!attrs.brand) {
+				return "Every product needs a brand.";
+			}
+			if(!attrs.name) {
+				return "Every product needs a name.";
+			}
+			if(attrs.price.original === "" && attrs.price.sale === "") {
+				return "Every product needs at least one price.";
+			}
+			if(attrs.price.original <= 0 || attrs.price.sale <= 0) {
+				return "Every product should have a value greater than zero.";
+			}
+			if(attrs.price.sale > attrs.price.original) {
+				return "The sale price should be less than original.";
+			}
+		},
 		defaults: {
 			quantity: 1,
-			price: {
-				original: "",
-				sale: ""
-			}
+			size: $(".size-selection")[0].value,
+			color: $(".color-selection")[0].value
 		},
   	initialize: function() {
     	console.log("A new product has been created")
@@ -87,9 +121,10 @@ var Product = Backbone.Model.extend({
 	 brand: "Sunset & Spring",
 	 name: "High/Low Maxi Dress",
 	 image: "<img src='https://images.bloomingdales.com/is/image/BLM/products/2/optimized/9497962_fpx.tif?wid=800&qlt=90,0&layer=comp&op_sharpen=0&resMode=sharp2&op_usm=0.7,1.0,0.5,0&fmt=jpeg'>",
+	 //this image value should actually point to an array
 	 price: {
-		 original: "$195",
-		 sale: "$145"
+		 original: 195,
+		 sale: 145
 	 }
  });
 
@@ -113,8 +148,8 @@ var Product = Backbone.Model.extend({
 		 this.$el.html(
 			 	"<p>" + this.model.get("brand") + "</p>" +
 		 		"<p>" + this.model.get("name") + "</p>" +
-				"<p>" + "Original Price: " + this.model.get("price").original + "</p>" +
-				"<p>" + "Sale: " + this.model.get("price").sale + "</p>" +
+				"<p>" + "Original Price: $" + this.model.get("price").original + "</p>" +
+				"<p>" + "Sale: $" + this.model.get("price").sale + "</p>" +
 				"<p>" + this.model.get("image") + "</p>")
 //how to get this view to render the new model attributes without showing these
 	//values as undefined before they are set?
