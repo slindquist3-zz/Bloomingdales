ed66a6b451f6e04f9057c9bb53443662afb28fc7
slindquist3 <slindquist3@gmail.com>
2017-05-24 17:39:56 -0400

    fix selection model methods

--- js/main.js
+++ js/main.js
@@ -1,45 +1,12 @@
-//adding an argument to all of these functions
-	//will make them more resuable
-
 
 $("#checkout").on('click', function() {
 	alert("Thanks for choosing Bloomingdale's!")
 })
 
-function total() {
-	return getSalePrice() * getQuantity();
-}
-
-function getQuantity() {
-	return parseInt(product.get("quantity"));
-}
-
-function getSalePrice() {
-	return parseInt(product.get("price").sale);
-}
-
-function getOriginalPrice() {
-	return parseInt(product.get("price").original);
-}
-
-function savings() {
-	var originalPrice = getOriginalPrice()
-	var quantity = getQuantity()
-	var wouldBePrice = originalPrice * quantity;
-	return wouldBePrice - total();
-}
-
-//I can refactor the above two functions into one, or atleast not repeat myself
-
-
 $('#next').on('click', function(e) {
 	e.preventDefault();
 	$("#selections").hide();
   $('#shipaddress').show();
-	$("#product-details").append("<p> Size: " + product.get("size") + "</p>");
-	$("#product-details").append("<p> Color: " + product.get("color") + "</p>");
-	$("#product-details").append("<p> Quantity: " + product.get("quantity") + "</p>");
-
 })
 
 $("#back").on('click', function() {
@@ -54,45 +21,44 @@ $("#edit").on('click', function() {
 })
 
 $('#finish').on('click', function() {
-	userinfo.set({
+	shipping.set({
 		address: $("#addOne").val() + " " + $("#addTwo").val(),
 		city: $("#city").val(),
 		state: $("#state").val(),
 		zipcode: $("#zipcode").val()
 	});
-	console.log(userinfo.attributes)
+	console.log(shipping.attributes)
 	$("#shipaddress").hide();
   $('#analyticsinfo').show();
-	$('#analyticsinfo').append("<p class='checkout'> Total: $" + total() + "</p>");
-	$('#analyticsinfo').append("<p class='checkout'> Your Savings: $" + savings(product) + "</p>");
-
-
-
+	$('#purchase').append("<p class='checkout'> Total: $" + selection.getTotal() + "</p>");
+	$('#purchase').append("<p class='checkout'> Your Savings: $" + selection.getSavings() + "</p>");
 })
 
 $(".size-selection").on("change", function() {
-	product.set(this.name, this.value);
-	console.log(product.attributes);
+	selection.set(this.name, this.value);
 })
 
 $(".color-selection").on("change", function() {
-	product.set(this.name, this.value)
-	if (product.get("color") === "Black") {
-		product.set("image", "<img class='dress' src='https://images.bloomingdales.com/is/image/BLM/products/2/optimized/9497962_fpx.tif?wid=800&qlt=90,0&layer=comp&op_sharpen=0&resMode=sharp2&op_usm=0.7,1.0,0.5,0&fmt=jpeg'>");
-	} else if (product.get("color")  === "Coral") {
-		product.set("image", "<img class='dress' src='https://images.bloomingdales.com/is/image/BLM/products/3/optimized/9497963_fpx.tif?wid=800&qlt=90,0&layer=comp&op_sharpen=0&resMode=sharp2&op_usm=0.7,1.0,0.5,0&fmt=jpeg'>");
-	} else if (product.get("color")  === "Blue") {
-		product.set("image", "<img class='dress' src='https://images.bloomingdales.com/is/image/BLM/products/6/optimized/9497966_fpx.tif?wid=800&qlt=90,0&layer=comp&op_sharpen=0&resMode=sharp2&op_usm=0.7,1.0,0.5,0&fmt=jpeg'>");
-	}
-	console.log(product.attributes);
+	selection.set("color", "to be set");
+	});
+
+$(".color-selection").on("change", function() {
+	selection.set(this.name, this.value);
+	var arr = Object.values(product.get("color"))
+	var url = []
+		for (var i = 0; i < arr.length; i++) {
+			if (Object.keys(arr[i])[0] === this.value) {
+				url.push(Object.values(arr[i])[0])
+			}
+		}
+	selection.set("image", url[0]);
+	console.log(selection.get("color"))
+	console.log(selection.get("image"))
+
 });
 
-//I can refactor this so that each image is just a value in an array and on color change the function iterates through the array of hypothetical images
-//These image values should all be stored on the instance of the model
-
 $(".quantity-selection").on("change", function() {
-	product.set(this.name, this.value);
-	console.log(product.attributes);
+	selection.set(this.name, this.value);
 });
 
 // ---------  MODELS -------------
@@ -115,11 +81,6 @@ var Product = Backbone.Model.extend({
 				return "The sale price should be less than original.";
 			}
 		},
-		defaults: {
-			quantity: 1,
-			size: $(".size-selection")[0].value,
-			color: $(".color-selection")[0].value
-		},
   	initialize: function() {
     	console.log("A new product has been created")
     }
@@ -128,22 +89,51 @@ var Product = Backbone.Model.extend({
  var product = new Product({
 	 brand: "Sunset & Spring",
 	 name: "High/Low Maxi Dress",
-	 image: "<img class='dress' src='https://images.bloomingdales.com/is/image/BLM/products/2/optimized/9497962_fpx.tif?wid=800&qlt=90,0&layer=comp&op_sharpen=0&resMode=sharp2&op_usm=0.7,1.0,0.5,0&fmt=jpeg'>",
 	 //this image value should actually point to an array
 	 price: {
 		 original: 195,
 		 sale: 145
-	 }
+	 },
+	 color:[
+		 {Black: "https://images.bloomingdales.com/is/image/BLM/products/2/optimized/9497962_fpx.tif?wid=800&qlt=90,0&layer=comp&op_sharpen=0&resMode=sharp2&op_usm=0.7,1.0,0.5,0&fmt=jpeg"},
+		 {Coral: "https://images.bloomingdales.com/is/image/BLM/products/3/optimized/9497963_fpx.tif?wid=800&qlt=90,0&layer=comp&op_sharpen=0&resMode=sharp2&op_usm=0.7,1.0,0.5,0&fmt=jpeg"},
+		 {Blue: "https://images.bloomingdales.com/is/image/BLM/products/6/optimized/9497966_fpx.tif?wid=800&qlt=90,0&layer=comp&op_sharpen=0&resMode=sharp2&op_usm=0.7,1.0,0.5,0&fmt=jpeg"}
+	 ],
+	 size: ["2S", "3M", "4L"]
  });
 
- var Userinfo = Backbone.Model.extend({
+ var Selection = Backbone.Model.extend({
+	 defaults: {
+		 quantity: 1,
+		 size: $(".size-selection")[0].value,
+		 color: $(".color-selection")[0].value,
+		 image: Object.values(product.get("color")[0])[0]
+	 },
 	 initialize: function() {
-		 console.log("User has info!")
+		 console.log("User can make selections.")
+	 },
+	 getQuantity: function() {
+		 return this.get("quantity");
+	 },
+	 getTotal: function() {
+		 return this.getQuantity() * product.get("price").sale;
+	 },
+	 getSavings: function() {
+		 return (this.getQuantity() * product.get("price").original) - this.getTotal();
 	 }
  });
 
- var userinfo = new Userinfo({
+ var selection = new Selection();
+
+ var Shipping = Backbone.Model.extend({
+	 initialize: function() {
+		 console.log("Shipping info!")
+	 }
  });
+
+ var shipping = new Shipping();
+
+
 
  // ---------  VIEWS -------------
 
@@ -154,12 +144,11 @@ var Product = Backbone.Model.extend({
 	 },
 	 render: function() {
 		 this.$el.html(
-			 	"<p>" + this.model.get("brand") + "</p>" +
+			 	"<p id='brand'><strong>" + this.model.get("brand") + "</strong></p>" +
 		 		"<p>" + this.model.get("name") + "</p>" +
-				"<p>" + "Original Price: $" + this.model.get("price").original + "</p>" +
-				"<p>" + "Sale: $" + this.model.get("price").sale + "</p>" +
-				"<p>" + this.model.get("image") + "</p>")
-		 return this;
+				"<p><strike>" + "REG. $" + this.model.get("price").original + "</strike></p>" +
+				"<p id='sale'>" + "SALE $" + this.model.get("price").sale + "</p>")
+		return this;
 	 }
  });
 
@@ -167,7 +156,7 @@ var Product = Backbone.Model.extend({
  productView.render();
 
 
- var UserinfoView = Backbone.View.extend({
+ var ShippingView = Backbone.View.extend({
 	 tagName: "div",
 	 initialize: function(){
 		 this.model.on("change", this.render, this)
@@ -182,19 +171,21 @@ var Product = Backbone.Model.extend({
 	 }
  })
 
- var userinfoView = new UserinfoView({ el: "#shipping-info", model: userinfo});
- userinfoView.render();
+ var shippingView = new ShippingView({ el: "#shipping-info", model: shipping});
+ shippingView.render();
  //There should be some way to use one function to populate my models with their attributes, because to do each
  //	one individually will make my code heavy and not very dry.
 
-//When a user clicks next, I need to also add the product to a hypothetical Product Collection,
-//  which will be populated only with this one model, but it will show that it could have others
+ var SelectionView = Backbone.View.extend({
+	 initialize: function(){
+		this.model.on("change", this.render, this)
+	},
+	render: function() {
+		this.$el.html(
+			 "<img id='mainImage' class='views' src=" + this.model.get("image") + ">");
+		return this;
+	}
+ })
 
-//That Product collection view will render the modelViews
-
-  //create product collection
-  //create product collection views
-
-
-  //need to handle events when on the last page a user wants to delete an item from the collection.
-  //integrate Bootstrap
+ var selectionView = new SelectionView({el: "#image-view", model: selection})
+ selectionView.render()
